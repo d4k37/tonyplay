cb60002a5982175a76933a27c01432395b88b036
Luís <59730229+d4k37@users.noreply.github.com>
2021-06-25 15:03:20 -0300

    up
    extensão do git bugando

--- src/configs/discordAuth.ts
+++ src/configs/discordAuth.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2F%40d4k37%2Ftonyplay'
+=======
+const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2Ftonyplay'
+>>>>>>> parent of 1efeb53 (up)
 const SCOPE = 'identify%20email%20connections%20guilds'
 const RESPONSE_TYPE = 'token'
 const CLIENT_ID ='857673773818183720'
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,6 +2,7 @@
 
 
 import React, 
+<<<<<<< HEAD
 { 
   createContext,
   useContext,
@@ -10,6 +11,14 @@ import React,
 } from 'react';
 
 import * as AuthSession from 'expo-auth-session';
+=======
+       {createContext,
+        useContext,
+        useState, 
+        ReactNode
+        } 
+from 'react'
+>>>>>>> parent of 1efeb53 (up)
 
 import {
   SCOPE,
@@ -46,11 +55,49 @@ type AuthorizationResponse = AuthSession.AuthSessionResult & {
   }
 }
 
+<<<<<<< HEAD
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User);
   const [loading, setLoading] = useState(false);
+=======
+export const AuthContext =  createContext({} as AuthcontextData);
+
+function AuthProvider({ children }: AuthProviderProps){
+    const [user, setUser] = useState<User>({} as User)
+    const [loading, setLoading] = useState(false)
+
+   async function signIn(){
+        try{
+            setLoading(true)
+
+            const authUrl = "https://discord.com/api/oauth2/authorize?client_id=857673773818183720&redirect_uri=https%3A%2F%2Fauth.expo.io%2Ftonyplay&response_type=code&scope=identify%20email%20connections%20guilds"
+            
+          
+            
+
+          const {type, params} =  await AuthSession.startAsync({authUrl }) as AuthorizationResponse
+
+          if(type === "success"){
+              api.defaults.headers.authorization = `Bearer ${params.access_token}`
+              const userInfo = await api.get('/users/@me')
+              const firstName = userInfo.data.username.split('')[0]
+              userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`
+
+              setUser({
+                  ...userInfo.data,
+                  firstName,
+                  token: params.access_token,
+              })
+            setLoading(false)
+          } else{ setLoading(false)}
+
+        } catch {
+            throw new Error('Não foi possível')
+        }
+    }
+>>>>>>> parent of 1efeb53 (up)
 
   async function signIn() {
     try {
